c119f500f95b6b120fbe552835a52123dd563dbd
Abrazax56 <94457621+Abrazax56@users.noreply.github.com>
2022-05-31 11:30:51 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -12,11 +12,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283160624440','6282279915237']
-global.premium = ['6282279915237']
-global.botname = 'Feyza Botz'
-global.ownername = 'Feyza Botz'
-global.packname = 'Feyza'
+global.owner = ['6288216018165','62882160181655']
+global.premium = ['6288216018165']
+global.botname = 'MD Trochesz'
+global.ownername = 'Ru Slyy'
+global.packname = 'By Abrazax'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
